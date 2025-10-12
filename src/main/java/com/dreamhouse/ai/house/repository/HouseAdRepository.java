ab4cf5ed298d77f84aac5d73cc5ba65652d8b57e
berkde <berkdelibalta@gmail.com>
2025-10-12 17:46:24 -0400

    Tagged a function input parameter with Param annotation as well as defined an entityGraph annotation for finding all houses function

--- src/main/java/com/dreamhouse/ai/house/repository/HouseAdRepository.java
+++ src/main/java/com/dreamhouse/ai/house/repository/HouseAdRepository.java
@@ -4,6 +4,7 @@ import com.dreamhouse.ai.house.model.entity.HouseAdEntity;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
 
@@ -13,12 +14,12 @@ import java.util.Optional;
 @Repository
 public interface HouseAdRepository extends JpaRepository<HouseAdEntity, Long> {
     @EntityGraph(attributePaths = {"images", "messages"})
-    Optional<HouseAdEntity> findByHouseAdUid(String houseAdId);
+    Optional<HouseAdEntity> findByHouseAdUid(@Param("uid") String houseAdId);
     Optional<HouseAdEntity> findByTitle(String title);
     List<HouseAdEntity> findAllByTitleContainingIgnoreCase(String title);
     List<HouseAdEntity> findAllByDescriptionContainingIgnoreCase(String description);
 
-    @EntityGraph(attributePaths = {"images"})   // add more if you need
+    @EntityGraph(attributePaths = {"images"})
     @Query("select h from HouseAdEntity h")
     @NonNull List<HouseAdEntity> findAll();
 }
