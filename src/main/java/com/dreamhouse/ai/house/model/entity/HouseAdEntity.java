7098caea7e67a5287a250637119202513ad028ea
berkde <berkdelibalta@gmail.com>
2025-10-14 21:09:36 -0400

    Updated the houseAdEntity class with new additional variable fields

--- src/main/java/com/dreamhouse/ai/house/model/entity/HouseAdEntity.java
+++ src/main/java/com/dreamhouse/ai/house/model/entity/HouseAdEntity.java
@@ -6,8 +6,11 @@ import jakarta.persistence.CascadeType;
 import jakarta.persistence.Index;
 import jakarta.persistence.OrderBy;
 import jakarta.persistence.Table;
+import org.hibernate.annotations.Array;
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
 
 import java.io.Serial;
 import java.io.Serializable;
@@ -37,8 +40,37 @@ public class HouseAdEntity implements Serializable {
     @Column(nullable = false)
     private String title;
 
-    @Column(columnDefinition = "text")
+    @Column(columnDefinition = "text", length = 10000)
     private String description;
+
+    private String city;
+
+    private String state;
+
+    private String neighborhood;
+
+    private Double price;
+
+    private Integer beds;
+
+    private Integer baths;
+
+    private Integer sqft;
+
+    private String type;
+
+    private Boolean parking;
+
+    private Boolean petsAllowed;
+
+    private Boolean waterfront;
+
+    private Integer yearBuilt;
+
+    @JdbcTypeCode(SqlTypes.VECTOR)
+    @Array(length = 768)
+    @Column(columnDefinition = "vector(768)")
+    private float[] embedding;
 
     @OneToMany(
             mappedBy = "houseAd",
@@ -57,6 +89,46 @@ public class HouseAdEntity implements Serializable {
     private Set<HouseAdMessageEntity> messages = new LinkedHashSet<>();
 
     public HouseAdEntity() {}
+
+    public HouseAdEntity(String houseAdUid,
+                         UserEntity user,
+                         String title,
+                         String description,
+                         String city,
+                         String state,
+                         String neighborhood,
+                         Double price,
+                         Integer beds,
+                         Integer baths,
+                         Integer sqft,
+                         String type,
+                         Boolean parking,
+                         Boolean petsAllowed,
+                         Boolean waterfront,
+                         Integer yearBuilt,
+                         float[] embedding,
+                         Set<HouseAdImageEntity> images,
+                         Set<HouseAdMessageEntity> messages) {
+        this.houseAdUid = houseAdUid;
+        this.user = user;
+        this.title = title;
+        this.description = description;
+        this.city = city;
+        this.state = state;
+        this.neighborhood = neighborhood;
+        this.price = price;
+        this.beds = beds;
+        this.baths = baths;
+        this.sqft = sqft;
+        this.type = type;
+        this.parking = parking;
+        this.petsAllowed = petsAllowed;
+        this.waterfront = waterfront;
+        this.yearBuilt = yearBuilt;
+        this.embedding = embedding;
+        this.images = images;
+        this.messages = messages;
+    }
 
     public void addImage(HouseAdImageEntity image) {
         if (image == null) return;
@@ -99,6 +171,110 @@ public class HouseAdEntity implements Serializable {
 
     public String getDescription() { return description; }
     public void setDescription(String description) { this.description = description; }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getNeighborhood() {
+        return neighborhood;
+    }
+
+    public void setNeighborhood(String neighborhood) {
+        this.neighborhood = neighborhood;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getBeds() {
+        return beds;
+    }
+
+    public void setBeds(Integer beds) {
+        this.beds = beds;
+    }
+
+    public Integer getBaths() {
+        return baths;
+    }
+
+    public void setBaths(Integer baths) {
+        this.baths = baths;
+    }
+
+    public Integer getSqft() {
+        return sqft;
+    }
+
+    public void setSqft(Integer sqft) {
+        this.sqft = sqft;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Boolean getParking() {
+        return parking;
+    }
+
+    public void setParking(Boolean parking) {
+        this.parking = parking;
+    }
+
+    public Boolean getPetsAllowed() {
+        return petsAllowed;
+    }
+
+    public void setPetsAllowed(Boolean petsAllowed) {
+        this.petsAllowed = petsAllowed;
+    }
+
+    public Boolean getWaterfront() {
+        return waterfront;
+    }
+
+    public void setWaterfront(Boolean waterfront) {
+        this.waterfront = waterfront;
+    }
+
+    public Integer getYearBuilt() {
+        return yearBuilt;
+    }
+
+    public void setYearBuilt(Integer yearBuilt) {
+        this.yearBuilt = yearBuilt;
+    }
+
+    public float[] getEmbedding() {
+        return embedding;
+    }
+
+    public void setEmbedding(float[] embedding) {
+        this.embedding = embedding;
+    }
 
     public Set<HouseAdImageEntity> getImages() { return images; }
     public void setImages(Set<HouseAdImageEntity> images) { this.images = images; }
