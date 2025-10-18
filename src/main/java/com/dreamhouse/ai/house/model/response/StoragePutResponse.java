a2c3b95cb9af4a6f6ed74c3a9a448128cb174b3d
berkde <berkdelibalta@gmail.com>
2025-10-17 23:10:09 -0400

    Updated the class name from StoragePutResult to StoragePutResponse

--- src/main/java/com/dreamhouse/ai/house/model/response/StoragePutResponse.java
+++ src/main/java/com/dreamhouse/ai/house/model/response/StoragePutResponse.java
@@ -2,5 +2,5 @@ package com.dreamhouse.ai.house.model.response;
 
 import io.micrometer.common.lang.Nullable;
 
-public record StoragePutResult(String key, String url, @Nullable String thumbnailUrl) {
+public record StoragePutResponse(String key, String url, @Nullable String thumbnailUrl) {
 }
