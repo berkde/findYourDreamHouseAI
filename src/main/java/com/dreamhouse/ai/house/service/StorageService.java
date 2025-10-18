b53d6eb8849ce1c3e6e42445800ffb6f04b9bdad
berkde <berkdelibalta@gmail.com>
2025-10-17 23:14:06 -0400

    Updated the return object type of putObject default factory method

--- src/main/java/com/dreamhouse/ai/house/service/StorageService.java
+++ src/main/java/com/dreamhouse/ai/house/service/StorageService.java
@@ -1,12 +1,12 @@
 package com.dreamhouse.ai.house.service;
 
-import com.dreamhouse.ai.house.model.response.StoragePutResult;
+import com.dreamhouse.ai.house.model.response.StoragePutResponse;
 
 import java.time.Duration;
 import java.util.Optional;
 
 public interface StorageService {
     Optional<String> presignedGetUrl(String key, Duration duration);
-    Optional<StoragePutResult> putObject(String key, byte[] bytes, String contentType);
+    Optional<StoragePutResponse> putObject(String key, byte[] bytes, String contentType);
     void deleteObject(String key);
 }
