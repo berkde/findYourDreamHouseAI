6c8fef95c308cec76ff1923a0acad531f237461e
berkde <berkdelibalta@gmail.com>
2025-10-17 23:17:25 -0400

    New exception type added to handle both on service level

---
+++ src/main/java/com/dreamhouse/ai/house/exception/HouseAdDeleteException.java
@@ -0,0 +1,7 @@
+package com.dreamhouse.ai.house.exception;
+
+public class HouseAdDeleteException extends RuntimeException {
+    public HouseAdDeleteException(String message) {
+        super(message);
+    }
+}
