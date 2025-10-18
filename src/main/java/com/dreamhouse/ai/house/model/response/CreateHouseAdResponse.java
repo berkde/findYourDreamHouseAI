c97260cf1a9233db9b4583abaa58df800323e8b5
berkde <berkdelibalta@gmail.com>
2025-10-17 23:09:34 -0400

    Updated the class name from CreateHouseAdResponseModel to CreateHouseAdResponse

--- src/main/java/com/dreamhouse/ai/house/model/response/CreateHouseAdResponse.java
+++ src/main/java/com/dreamhouse/ai/house/model/response/CreateHouseAdResponse.java
@@ -2,12 +2,12 @@ package com.dreamhouse.ai.house.model.response;
 
 import java.util.Objects;
 
-public record CreateHouseAdResponseModel(String houseAdId, String title) {
-    public CreateHouseAdResponseModel {}
+public record CreateHouseAdResponse(String houseAdId, String title) {
+    public CreateHouseAdResponse {}
 
     @Override
     public boolean equals(Object o) {
-        if (!(o instanceof CreateHouseAdResponseModel(String adId, String title1))) return false;
+        if (!(o instanceof CreateHouseAdResponse(String adId, String title1))) return false;
         return Objects.equals(title, title1) && Objects.equals(houseAdId, adId);
     }
 
