786b0f52b23ac8d9a632c6bbb91c94c5375fa8dc
berkde <berkdelibalta@gmail.com>
2025-11-01 19:12:19 -0400

    Discarded import with * notation to avoid importing all the library classes, and instead only imported the used classes.

--- src/main/java/com/dreamhouse/ai/llm/controller/AiController.java
+++ src/main/java/com/dreamhouse/ai/llm/controller/AiController.java
@@ -11,12 +11,7 @@ import org.springframework.boot.actuate.endpoint.annotation.WriteOperation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Map;
