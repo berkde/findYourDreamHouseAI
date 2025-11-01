e8d1e5f17c6ff1cd8d4b464ffd8333621dee9d2f
berkde <berkdelibalta@gmail.com>
2025-11-01 19:21:02 -0400

    Adjusted the Redisson Client with fine grained configuration rules to account for over exceeding memory occupation during lock retrieval and release across the whole application during a single session.

--- src/main/java/com/dreamhouse/ai/llm/configuration/RedissonConfiguration.java
+++ src/main/java/com/dreamhouse/ai/llm/configuration/RedissonConfiguration.java
@@ -8,10 +8,26 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RedissonConfiguration {
-    @Bean
+    @Bean(destroyMethod = "shutdown")
     public RedissonClient redissonClient() {
+        String address = System.getenv("REDIS_SERVER");
         Config config = new Config();
-        config.useSingleServer().setAddress(System.getenv("REDIS_SERVER"));
+
+        config.setThreads(2);
+        config.setNettyThreads(2);
+
+        config.useSingleServer()
+                .setAddress(address)
+                .setConnectionMinimumIdleSize(5)
+                .setConnectionPoolSize(16)
+                .setTimeout(3000)
+                .setConnectTimeout(3000)
+                .setRetryAttempts(2)
+                .setRetryInterval(1500)
+                .setPingConnectionInterval(15000)
+                .setTcpNoDelay(true)
+                .setKeepAlive(true);
+
         return Redisson.create(config);
     }
 }
