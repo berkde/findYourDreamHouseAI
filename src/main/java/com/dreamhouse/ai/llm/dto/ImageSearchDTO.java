57fc0d386321d195657a51c3fb903c2b05cefc0e
berkde <berkdelibalta@gmail.com>
2025-11-03 11:46:49 -0500

    Changed the name of return object class to ImageSearchDTO

--- src/main/java/com/dreamhouse/ai/llm/dto/ImageSearchDTO.java
+++ src/main/java/com/dreamhouse/ai/llm/dto/ImageSearchDTO.java
@@ -1,17 +1,16 @@
-package com.dreamhouse.ai.llm.model.reply;
+package com.dreamhouse.ai.llm.dto;
 
 import com.dreamhouse.ai.house.dto.HouseAdDTO;
 import org.springframework.util.StringUtils;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
 
 
-public record ImageSearchReply(String inferredDescription,
-                               float[] queryVector,
-                               List<HouseAdDTO> results) {
-    public ImageSearchReply {
+public record ImageSearchDTO(String inferredDescription,
+                             float[] queryVector,
+                             List<HouseAdDTO> results) {
+    public ImageSearchDTO {
         if (results == null || results.isEmpty()) {
             throw new IllegalArgumentException("Results cannot be null or empty");
         }
@@ -24,17 +23,6 @@ public record ImageSearchReply(String inferredDescription,
 
         queryVector = Arrays.copyOf(queryVector, queryVector.length);
         results = List.copyOf(results);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (!(o instanceof ImageSearchReply(String description, float[] vector, List<HouseAdDTO> results1))) return false;
-        return Objects.deepEquals(queryVector, vector) && List.of(results).equals(results1) && Objects.equals(inferredDescription, description);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(inferredDescription, Arrays.hashCode(queryVector), results);
     }
 
     @Override
