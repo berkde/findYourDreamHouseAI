493135e546af497f9b6dddfd9bfd8bb2a0af493a
berkde <berkdelibalta@gmail.com>
2025-11-03 11:43:37 -0500

    Updated the object return type of the search function, as well as updated the inner logic to return either text or json response based on user queries.

--- src/main/java/com/dreamhouse/ai/llm/controller/AiController.java
+++ src/main/java/com/dreamhouse/ai/llm/controller/AiController.java
@@ -1,15 +1,22 @@
 package com.dreamhouse.ai.llm.controller;
 
-import com.dreamhouse.ai.llm.model.reply.HouseSearchReply;
-import com.dreamhouse.ai.llm.service.agent.HouseSearchAgent;
-import com.dreamhouse.ai.llm.service.agent.ImageSearchAgent;
+import com.dreamhouse.ai.llm.model.reply.ChatReply;
+import com.dreamhouse.ai.llm.dto.HouseSearchDTO;
+import com.dreamhouse.ai.llm.agent.HouseSearchAgent;
+import com.dreamhouse.ai.llm.agent.ImageSearchAgent;
+import com.dreamhouse.ai.llm.model.reply.ListingsReply;
+import com.dreamhouse.ai.llm.model.reply.SearchReply;
+import com.dreamhouse.ai.llm.model.request.ChatRequest;
 import com.dreamhouse.ai.llm.service.impl.ImageSimilaritySearchServiceImpl;
 import com.dreamhouse.ai.llm.util.AIUtil;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.actuate.endpoint.annotation.WriteOperation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -38,10 +45,16 @@ public class AiController {
 
     @WriteOperation
     @PreAuthorize("hasRole('ADMIN') or hasRole('USER')")
-    @PostMapping("/search")
-    public ResponseEntity<HouseSearchReply> search(@RequestBody Map<String, String> body) {
-        String query = body.getOrDefault("q", "");
-        return ResponseEntity.ok(houseSearchAgent.chat(query));
+    @PostMapping(value = "/search")
+    public ResponseEntity<SearchReply> search(@AuthenticationPrincipal Authentication authentication,
+                                              @Valid @RequestBody ChatRequest request) {
+        String query = request.query();
+        String sessionId = String.valueOf((authentication.getPrincipal())).toLowerCase();
+        HouseSearchDTO reply = houseSearchAgent.chat(sessionId, query);
+        boolean chatOnly = reply.getHouseAdDTOS().isEmpty();
+
+        return chatOnly ? ResponseEntity.ok(new ChatReply(reply.getAgentReply())) :
+                          ResponseEntity.ok(new ListingsReply(reply.getHouseAdDTOS(), reply.getSummary()));
     }
 
     @WriteOperation
