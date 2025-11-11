e7eff989a7d850ac694af50935f5753b000e0396
berkde <berkdelibalta@gmail.com>
2025-11-11 15:29:44 -0500

    Updated the class with equals/hashCode

--- src/main/java/com/dreamhouse/ai/listener/event/UserRegisteredEvent.java
+++ src/main/java/com/dreamhouse/ai/listener/event/UserRegisteredEvent.java
@@ -6,4 +6,15 @@ public record UserRegisteredEvent(String username) {
     public UserRegisteredEvent {
         Objects.requireNonNull(username, "username cannot be null");
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof UserRegisteredEvent(String username1))) return false;
+        return Objects.equals(username, username1);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(username);
+    }
 }
