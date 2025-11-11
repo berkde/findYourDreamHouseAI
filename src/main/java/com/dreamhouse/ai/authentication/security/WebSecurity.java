65870d88f9cf00f4d69885237a74c200d45b699a
berkde <berkdelibalta@gmail.com>
2025-11-11 12:05:20 -0500

    Annotated the class with enable global authentication decorator to allow application wide authentication context access

--- src/main/java/com/dreamhouse/ai/authentication/security/WebSecurity.java
+++ src/main/java/com/dreamhouse/ai/authentication/security/WebSecurity.java
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -28,6 +29,7 @@ import javax.crypto.SecretKey;
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
+@EnableGlobalAuthentication
 public class WebSecurity {
     private final BCryptPasswordEncoder passwordEncoder;
     private final UserServiceImpl userService;
